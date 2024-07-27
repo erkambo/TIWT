ebc58745dde1eb6834fe96eeb27197384d2a112a
erkam <boyaciogluerkam@gmail.com>
2024-07-27 15:01:29 -0400

    Google Authentication
    Yes

--- app.py
+++ app.py
@@ -1,43 +1,83 @@
-import flask,requests,google, oauthlib
-from oauthlib.oauth2 import WebApplicationClient
 import os
+import pathlib
 
-from flask import Flask, url_for, render_template, redirect
+import requests
+from flask import Flask, session, abort, redirect, request, render_template
+from google.oauth2 import id_token
+from google_auth_oauthlib.flow import Flow
+from pip._vendor import cachecontrol
+import google.auth.transport.requests
 
-from flask_login import (
-    LoginManager,
-    current_user,
-    login_required,
-    login_user,
-    logout_user,
+app = Flask("Google Login App")
+app.secret_key = "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" # make sure this matches with that's in client_secret.json
+
+os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1" # to allow Http traffic for local dev
+
+GOOGLE_CLIENT_ID = "255099809593-78h1jr7mubuto6s7iern9ebdf0l14umk.apps.googleusercontent.com"
+client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret.json")
+
+flow = Flow.from_client_secrets_file(
+    client_secrets_file=client_secrets_file,
+    scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid"],
+    redirect_uri="http://127.0.0.1:5000/callback"
 )
-from oauthlib.oauth2 import WebApplicationClient
-app = Flask(__name__)
 
 
-# Configuration
-GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-GOOGLE_DISCOVERY_URL = (
-    "https://accounts.google.com/.well-known/openid-configuration"
-)
+def login_is_required(function):
+    def wrapper(*args, **kwargs):
+        if "google_id" not in session:
+            return abort(401)  # Authorization required
+        else:
+            return function()
 
-# OAuth 2 client setup
-client = WebApplicationClient(GOOGLE_CLIENT_ID)
+    return wrapper
 
-@app.route("/") #Home page
-def home():
+
+@app.route("/login")
+def login():
+    authorization_url, state = flow.authorization_url()
+    session["state"] = state
+    return redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    flow.fetch_token(authorization_response=request.url)
+
+    if not session["state"] == request.args["state"]:
+        abort(500)  # State does not match!
+
+    credentials = flow.credentials
+    request_session = requests.session()
+    cached_session = cachecontrol.CacheControl(request_session)
+    token_request = google.auth.transport.requests.Request(session=cached_session)
+
+    id_info = id_token.verify_oauth2_token(
+        id_token=credentials._id_token,
+        request=token_request,
+        audience=GOOGLE_CLIENT_ID
+    )
+
+    session["google_id"] = id_info.get("sub")
+    session["name"] = id_info.get("name")
+    return redirect("/goals")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/")
+
+
+@app.route("/")
+def index():
     return render_template("menu.html")
 
-@app.route("/goals") #goal setting
-def goals():
-    return render_template("goals.html")
 
-@app.route("/login") #google login
-def login():
+@app.route("/goals")
+@login_is_required
+def protected_area():
+    return render_template("goals.html",names = session['name']) 
 
-@app.route("/logout") #google logout
-def logout():
-
-@app.route("/login/callback") #google callback
-def callback():
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=80, debug=True)
