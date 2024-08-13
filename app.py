4e72b8668bc7560ef5054104c2e76fcd7d170f7f
erkam <boyaciogluerkam@gmail.com>
2024-08-13 15:36:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ client_secrets_file = os.path.join(pathlib.Path(__file__).parent, "client_secret
 flow = Flow.from_client_secrets_file(
     client_secrets_file=client_secrets_file,
     scopes=["https://www.googleapis.com/auth/userinfo.profile", "https://www.googleapis.com/auth/userinfo.email", "openid", "https://www.googleapis.com/auth/calendar"],
-    redirect_uri="http://127.0.0.1/callback", 
+    redirect_uri="https://tiwt-430621.ue.r.appspot.com/callback", 
 )
 
 
